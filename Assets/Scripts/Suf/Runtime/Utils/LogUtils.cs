750294ec6bfe6fcf09e86c5cbe8fa471dbe59b50
zfkun <zfkun@msn.com>
2022-12-10 15:44:43 +0800

    up

--- Assets/Scripts/Suf/Runtime/Utils/LogUtils.cs
+++ Assets/Scripts/Suf/Runtime/Utils/LogUtils.cs
@@ -4,40 +4,46 @@ namespace Suf.Utils
 {
     public static class LogUtils
     {
-        [Conditional("ENABLE_LOG")]
+        [Conditional("UNITY_EDITOR")]
+        [Conditional("SUF_ENABLE_LOG")]
         public static void Info(object message, UnityEngine.Object context = null)
         {
             UnityEngine.Debug.Log("[I] " + System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffff") +" " + message, context);
         }
         
-        [Conditional("ENABLE_LOG")]
+        [Conditional("UNITY_EDITOR")]
+        [Conditional("SUF_ENABLE_LOG")]
         public static void InfoFormat(string format, params object[]args)
         {
-            UnityEngine.Debug.LogFormat(format, args);
+            UnityEngine.Debug.LogFormat("[I] " + format, args);
         }
         
-        [Conditional("ENABLE_LOG")]
+        [Conditional("UNITY_EDITOR")]
+        [Conditional("SUF_ENABLE_LOG")]
         public static void Warn(object message, UnityEngine.Object context = null)
         {
             UnityEngine.Debug.LogWarning("[W] " + System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffff") +" " + message, context);
         }
         
-        [Conditional("ENABLE_LOG")]
+        [Conditional("UNITY_EDITOR")]
+        [Conditional("SUF_ENABLE_LOG")]
         public static void WarningFormat(string format, params object[]args)
         {
-            UnityEngine.Debug.LogWarningFormat(format, args);
+            UnityEngine.Debug.LogWarningFormat("[W] " + format, args);
         }
         
-        [Conditional("ENABLE_LOG")]
+        [Conditional("UNITY_EDITOR")]
+        [Conditional("SUF_ENABLE_LOG")]
         public static void Error(object message, UnityEngine.Object context = null)
         {
             UnityEngine.Debug.LogError("[E] " + System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffff") +" " + message, context);
         }
         
-        [Conditional("ENABLE_LOG")]
+        [Conditional("UNITY_EDITOR")]
+        [Conditional("SUF_ENABLE_LOG")]
         public static void ErrorFormat(string format, params object[]args)
         {
-            UnityEngine.Debug.LogErrorFormat(format, args);
+            UnityEngine.Debug.LogErrorFormat("[E] " + format, args);
         }
     }
 }
